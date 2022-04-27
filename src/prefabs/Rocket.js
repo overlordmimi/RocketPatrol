824f29a3953328fb98adee32cc09166d2ada380c
overlordmimi <103212272+overlordmimi@users.noreply.github.com>
2022-04-26 17:57:53 -0700

    everything finished
    includes everything demo did

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -7,6 +7,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
       scene.add.existing(this);
       this.isFiring = false;
       this.moveSpeed = 2;
+      this.sfxRocket = scene.sound.add('sfx_rocket');
     }
 
     update() {
@@ -21,7 +22,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
         // fire button
         if(Phaser.Input.Keyboard.JustDown(keyF)) {
             this.isFiring = true;
-            //this.sfxRocket.play();
+            this.sfxRocket.play();  // play sfx
         }
         // if fired, move up
         if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
